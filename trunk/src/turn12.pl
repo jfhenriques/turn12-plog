90d810c910149958bcd4b577a200aea596c62df2
joaoh88@gmail.com <joaoh88@gmail.com@d433ada3-79d1-79d4-bf46-8ee9486c25bd>
2011-12-07 18:47:35 +0000

    fix outra vez.

--- trunk/src/turn12.pl
+++ trunk/src/turn12.pl
@@ -6,25 +6,24 @@
 get_elements_pos( _,  _, -1,-1,-1,-1,   A,B,C,D,   A,B,C,D   ):-!.
 
 get_elements_pos( [X|T],  Pos,  Pos,P2,P3,P4,   _,B,C,D,   E,F,G,H   ) :- !,
-		Pos1 is Pos - 1,
-		get_elements_pos( T,  Pos1,  -1,P2,P3,P4,   X,B,C,D,   E,F,G,H   ).
+	Pos1 is Pos - 1,
+	get_elements_pos( T,  Pos1,  -1,P2,P3,P4,   X,B,C,D,   E,F,G,H   ).
 		
 get_elements_pos( [X|T],  Pos,  P1,Pos,P3,P4,   A,_,C,D,   E,F,G,H   ) :- !,
-		Pos1 is Pos - 1,
-		get_elements_pos( T,  Pos1,  P1,-1,P3,P4,   A,X,C,D,   E,F,G,H   ).
+	Pos1 is Pos - 1,
+	get_elements_pos( T,  Pos1,  P1,-1,P3,P4,   A,X,C,D,   E,F,G,H   ).
 
 get_elements_pos( [X|T],  Pos,  P1,P2,Pos,P4,   A,B,_,D,   E,F,G,H   ) :- !,
-		Pos1 is Pos - 1,
-		get_elements_pos( T,  Pos1,  P1,P2,-1,P4,   A,B,X,D,   E,F,G,H   ).
-		
+	Pos1 is Pos - 1,
+	get_elements_pos( T,  Pos1,  P1,P2,-1,P4,   A,B,X,D,   E,F,G,H   ).
+	
 get_elements_pos( [X|T],  Pos,  P1,P2,P3,Pos,   A,B,C,_,   E,F,G,H   ) :- !,
-		Pos1 is Pos - 1,
-		get_elements_pos( T,  Pos1,  P1,P2,P3,-1,   A,B,C,X,   E,F,G,H   ).
+	Pos1 is Pos - 1,
+	get_elements_pos( T,  Pos1,  P1,P2,P3,-1,   A,B,C,X,   E,F,G,H   ).
 		
 get_elements_pos( [_|T],  Pos,  P1,P2,P3,P4,   A,B,C,D,   E,F,G,H   ) :- !,
-		Pos1 is Pos - 1,
-		%write(P1), write(','), write(P2), write(','), write(P3), write(','), write(P4), nl,
-		get_elements_pos( T,  Pos1,  P1,P2,P3,P4,   A,B,C,D,   E,F,G,H   ).
+	Pos1 is Pos - 1,
+	get_elements_pos( T,  Pos1,  P1,P2,P3,P4,   A,B,C,D,   E,F,G,H   ).
 		
 		
 /*print_time:-
@@ -48,12 +47,12 @@ print_face_cross(FaceN,C1,C2,C3,C4):-
 
 
 shifted_face( Face, Shift, TotalElements, Distance, C1, C2, C3, C4 ) :- !,
-		S_F1 is ( Shift mod TotalElements ),
-		S_F2 is ( ( Shift + Distance ) mod TotalElements ),
-		S_F3 is ( ( Shift + ( 2 * Distance ) ) mod TotalElements ),
-		S_F4 is ( ( Shift + ( 3 * Distance ) ) mod TotalElements ),
-		TotalElementsIn is TotalElements - 1,
-		get_elements_pos( Face,  TotalElementsIn,   S_F1,S_F2,S_F3,S_F4,  0,0,0,0,  C1,C2,C3,C4 ).
+	S_F1 is ( Shift mod TotalElements ),
+	S_F2 is ( ( Shift + Distance ) mod TotalElements ),
+	S_F3 is ( ( Shift + ( 2 * Distance ) ) mod TotalElements ),
+	S_F4 is ( ( Shift + ( 3 * Distance ) ) mod TotalElements ),
+	TotalElementsIn is TotalElements - 1,
+	get_elements_pos( Face,  TotalElementsIn,   S_F1,S_F2,S_F3,S_F4,  0,0,0,0,  C1,C2,C3,C4 ).
 			
 print_rots(R1,R2,R3,R4,R5,R6) :-
 	write('R_TT: '), write( R1 ), nl,
@@ -80,7 +79,7 @@ w_middle_nums(TREE_L_DESC,N1,N2):- write('|'),write(N1),write('  ') , write(TREE
 w_cube_end:-write('|').
 
 project_cube(  TT_C1,TT_C2,TT_C3,TT_C4,   BA_C1,BA_C2,BA_C3,BA_C4,   RR_C1,RR_C2,RR_C3,RR_C4,
-	           LL_C1,LL_C2,LL_C3,LL_C4,   FF_C1,FF_C2,FF_C3,FF_C4,   BO_C1,BO_C2,BO_C3,BO_C4 ) :-
+               LL_C1,LL_C2,LL_C3,LL_C4,   FF_C1,FF_C2,FF_C3,FF_C4,   BO_C1,BO_C2,BO_C3,BO_C4 ) :-
 	
 	% BACK
 	w_side_space,                       w_top_line,                         nl,
