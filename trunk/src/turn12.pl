b3afcb155e8408d9cdf42a83546c3887df226158
joaoh88@gmail.com <joaoh88@gmail.com@d433ada3-79d1-79d4-bf46-8ee9486c25bd>
2011-12-07 18:49:47 +0000

    apagadas coisas desnecessárias.

--- trunk/src/turn12.pl
+++ trunk/src/turn12.pl
@@ -1,41 +1,25 @@
 
 :- use_module(library(clpfd)).
-%:- use_module(library(system)).
 
 
 get_elements_pos( _,  _, -1,-1,-1,-1,   A,B,C,D,   A,B,C,D   ):-!.
-
 get_elements_pos( [X|T],  Pos,  Pos,P2,P3,P4,   _,B,C,D,   E,F,G,H   ) :- !,
 	Pos1 is Pos - 1,
 	get_elements_pos( T,  Pos1,  -1,P2,P3,P4,   X,B,C,D,   E,F,G,H   ).
-		
 get_elements_pos( [X|T],  Pos,  P1,Pos,P3,P4,   A,_,C,D,   E,F,G,H   ) :- !,
 	Pos1 is Pos - 1,
 	get_elements_pos( T,  Pos1,  P1,-1,P3,P4,   A,X,C,D,   E,F,G,H   ).
-
 get_elements_pos( [X|T],  Pos,  P1,P2,Pos,P4,   A,B,_,D,   E,F,G,H   ) :- !,
 	Pos1 is Pos - 1,
 	get_elements_pos( T,  Pos1,  P1,P2,-1,P4,   A,B,X,D,   E,F,G,H   ).
-	
 get_elements_pos( [X|T],  Pos,  P1,P2,P3,Pos,   A,B,C,_,   E,F,G,H   ) :- !,
 	Pos1 is Pos - 1,
 	get_elements_pos( T,  Pos1,  P1,P2,P3,-1,   A,B,C,X,   E,F,G,H   ).
-		
 get_elements_pos( [_|T],  Pos,  P1,P2,P3,P4,   A,B,C,D,   E,F,G,H   ) :- !,
 	Pos1 is Pos - 1,
 	get_elements_pos( T,  Pos1,  P1,P2,P3,P4,   A,B,C,D,   E,F,G,H   ).
-		
-		
-/*print_time:-
-	datime(T),
-	write(T).
-		nl, print_time, nl,
-	now(EndRun),
-	TotalTime is (EndRun - StartRun),
-	write('Total time is: '),
-	write(TotalTime), nl,*/
 
-
+	
 print_face_cross(FaceN,C1,C2,C3,C4):-
 	write('['),
 	write(FaceN),
@@ -53,7 +37,8 @@ shifted_face( Face, Shift, TotalElements, Distance, C1, C2, C3, C4 ) :- !,
 	S_F4 is ( ( Shift + ( 3 * Distance ) ) mod TotalElements ),
 	TotalElementsIn is TotalElements - 1,
 	get_elements_pos( Face,  TotalElementsIn,   S_F1,S_F2,S_F3,S_F4,  0,0,0,0,  C1,C2,C3,C4 ).
-			
+
+	
 print_rots(R1,R2,R3,R4,R5,R6) :-
 	write('R_TT: '), write( R1 ), nl,
 	write('R_BA: '), write( R2 ), nl,
@@ -61,6 +46,7 @@ print_rots(R1,R2,R3,R4,R5,R6) :-
 	write('R_LL: '), write( R4 ), nl,
 	write('R_FF: '), write( R5 ), nl,
 	write('R_BO: '), write( R6 ), nl.
+
 	
 string_to_list(String,ListOut):-!,
 	string_to_list(String,[],ListOut).
@@ -68,6 +54,7 @@ string_to_list([H|T],ListIn,ListOut):-!,
 	Val is H - 48, % o número zero tem o código ascii 48
 	string_to_list(T,[Val|ListIn],ListOut).
 string_to_list(_,List,List):-!.
+
 
 w_top_line:- write(' _________').
 w_top_line_f:- write('_________').
@@ -77,6 +64,7 @@ w_bot_num_line(N):- write('|____'),write(N),write('____').
 w_empty_line:- write('|         ').
 w_middle_nums(TREE_L_DESC,N1,N2):- write('|'),write(N1),write('  ') , write(TREE_L_DESC), write('  '),write(N2).
 w_cube_end:-write('|').
+
 
 project_cube(  TT_C1,TT_C2,TT_C3,TT_C4,   BA_C1,BA_C2,BA_C3,BA_C4,   RR_C1,RR_C2,RR_C3,RR_C4,
                LL_C1,LL_C2,LL_C3,LL_C4,   FF_C1,FF_C2,FF_C3,FF_C4,   BO_C1,BO_C2,BO_C3,BO_C4 ) :-
@@ -103,7 +91,7 @@ project_cube(  TT_C1,TT_C2,TT_C3,TT_C4,   BA_C1,BA_C2,BA_C3,BA_C4,   RR_C1,RR_C2
 	w_side_space,                       w_empty_line,                       w_cube_end, nl,
 	w_side_space,                       w_bot_num_line(FF_C3),              w_cube_end, nl.
 
-		
+
 turn12:-
 	
 	string_to_list( "356735869637537564374649", Top    ),
@@ -119,38 +107,32 @@ turn12:-
 	Rotations = [ R1, R2, R3, R4, R5, R6 ],
 
 	domain(Rotations, 0, 23),
-	
 
 	labeling([], [R1]),
 	shifted_face( Top   , R1, TotElems, Distance, TT_C1, TT_C2, TT_C3, TT_C4 ),
 	labeling([], [R2]),
 	shifted_face( Back  , R2, TotElems, Distance, BA_C1, BA_C2, BA_C3, BA_C4 ),
-	%print_rots(1, R1,R2,0,0,0,0),
 	
 	TT_C1 + BA_C3 #= 12,
 
 	labeling([], [R3]),
 	shifted_face( Right , R3, TotElems, Distance, RR_C1, RR_C2, RR_C3, RR_C4 ),
-	%print_rots(2, R1,R2,R3,0,0,0),
 	TT_C2 + RR_C4 #= 12,
 	RR_C1 + BA_C2 #= 12,
 	
 	labeling([], [R4]),
 	shifted_face( Left  , R4, TotElems, Distance, LL_C1, LL_C2, LL_C3, LL_C4 ),
-	%print_rots(3, R1,R2,R3,R4,0,0),
 	TT_C4 + LL_C2 #= 12,
 	LL_C1 + BA_C4 #= 12,
 	
 	labeling([], [R5]),
 	shifted_face( Front , R5, TotElems, Distance, FF_C1, FF_C2, FF_C3, FF_C4 ),
-	%print_rots(4, R1,R2,R3,R4,R5,0),
 	TT_C3 + FF_C1 #= 12,
 	RR_C3 + FF_C2 #= 12,
 	LL_C3 + FF_C4 #= 12,
 
 	labeling([], [R6]),
 	shifted_face( Bottom, R6, TotElems, Distance, BO_C1, BO_C2, BO_C3, BO_C4 ),
-	%print_rots(5, R1,R2,R3,R4,R5,R6),
 	BO_C3 + FF_C3 #= 12,
 	BO_C2 + LL_C4 #= 12,
 	BO_C4 + RR_C2 #= 12,
@@ -158,20 +140,11 @@ turn12:-
 	
 	
 	print_rots( R1,R2,R3,R4,R5,R6 ),
-	
-	/*print_face_cross('Top   ', TT_C1, TT_C2, TT_C3, TT_C4 ),
-	print_face_cross('Back  ', BA_C1, BA_C2, BA_C3, BA_C4 ),
-	print_face_cross('Right ', RR_C1, RR_C2, RR_C3, RR_C4 ),
-	print_face_cross('Left  ', LL_C1, LL_C2, LL_C3, LL_C4 ),
-	print_face_cross('Front ', FF_C1, FF_C2, FF_C3, FF_C4 ),
-	print_face_cross('Bottom', BO_C1, BO_C2, BO_C3, BO_C4 ),*/
-	
 	nl,
 	
 	project_cube( TT_C1,TT_C2,TT_C3,TT_C4,   BA_C1,BA_C2,BA_C3,BA_C4,   RR_C1,RR_C2,RR_C3,RR_C4,
-	              LL_C1,LL_C2,LL_C3,LL_C4,   FF_C1,FF_C2,FF_C3,FF_C4,   BO_C1,BO_C2,BO_C3,BO_C4 ),
+                  LL_C1,LL_C2,LL_C3,LL_C4,   FF_C1,FF_C2,FF_C3,FF_C4,   BO_C1,BO_C2,BO_C3,BO_C4 ),
 	
 	nl,nl,
 	write('More possibilities ?'),
 	1 = 2. % falha para ver se existem mais possibilidades
-	
